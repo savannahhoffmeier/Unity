3a5c4b9acb771ad567a1b1bcc74fac6fb6c6fa73
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-09-10 15:45:56 -0600

    Scripting PowerUp

---
+++ Unity Scripting/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+ public int score;
+
+ private void OnTriggerEnter(Collider other)
+ {
+  score++
+ }
+}
