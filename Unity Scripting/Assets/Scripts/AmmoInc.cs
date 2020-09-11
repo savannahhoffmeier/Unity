b5ac0b674e93d6c131ccf52e001b01159b1741c1
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-09-11 13:44:33 -0600

    Healt and Ammo PowerUp

---
+++ Unity Scripting/Assets/Scripts/AmmoInc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoInc : MonoBehaviour
+{
+    public int ammo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ammo++;
+            print(ammo);
+    }
+}
