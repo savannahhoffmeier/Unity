5d95cdfe4eccc4771a4438d0e99c4d544cc0b78d
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-09-10 21:16:10 -0600

    Scripting PlayerMovement

--- Unity Scripting/Assets/Scripts/PowerUp.cs
+++ Unity Scripting/Assets/Scripts/PowerUp.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PowerUp : MonoBehaviour
 {
  public int score;
 
  private void OnTriggerEnter(Collider other)
  {
-  score++
+  score++;
+  print(score);
  }
 }
