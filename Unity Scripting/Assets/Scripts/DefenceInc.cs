3ff9d37a9a81374c07d3024b18c37975e8c8987c
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-09-12 13:18:09 -0600

    Scripting Defence File

--- Unity Scripting/Assets/Scripts/DefenceInc.cs
+++ Unity Scripting/Assets/Scripts/DefenceInc.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DefenceInc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int defence;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        defence++;
+        print(defence);
     }
 }
