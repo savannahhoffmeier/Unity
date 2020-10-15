991232704b85279c1080cdfe4fb08b73233cf32b
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-15 11:31:09 -0600

    FloatData

---
+++ Unity Scripting/Assets/Scripts/FloatData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public void UpdateValue(float number)
+    {
+        value += number;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = value;
+    }
+}
