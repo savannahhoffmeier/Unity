8995715f7fc04d30c78a5b9b1483b0c8a50088b2
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-20 15:46:24 -0600

    Shortening Code

---
+++ Unity Scripting/Assets/Scripts/BoolData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoolData : MonoBehaviour
+{
+    public bool value;
+    public void UpdateValue(bool newValue)
+    {
+        value = newValue;
+        Debug.Log("Game Over");
+    }
+}
