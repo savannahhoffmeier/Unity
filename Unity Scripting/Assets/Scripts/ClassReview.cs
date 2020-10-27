128f178c90d02fa36afbfe3cc915969b2853b1e9
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-27 16:54:39 -0600

    review

---
+++ Unity Scripting/Assets/Scripts/ClassReview.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class ClassReview : MonoBehaviour
+{
+    public int score;
+    public float health;
+    public string playerName;
+    public bool canPlay;
+    public GameObject playerArt;
+    public Transform playerTransform;
+
+    private CharacterController _controller;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = 3f;
+        _controller.Move(movement);
+    }
+}
