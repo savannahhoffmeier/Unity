8b716211206ce57faf83005a6d0551f0154c50bc
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-03 15:14:20 -0700

    class

---
+++ Unity Scripting/Assets/classScripts/FlipObjectBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipObjectBehavior : MonoBehaviour
+{
+    
+    public float rotateValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if ( Input.GetKey(KeyCode.LeftArrow)|| Input.GetKey(KeyCode.A))
+        {
+            rotateValue = 180;
+        }
+        if ( Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.D))
+        {
+            rotateValue = 0;
+        }
+        transform.rotation=Quaternion.Euler(0,rotateValue,0);
+    }
+}
---
+++ Unity Scripting/Assets/classScripts/GameAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameAction : ScriptableObject
+{
+    public UnityAction action;
+    // Start is called before the first frame update
+    public void Raise()
+    {
+        action.Invoke();
+    }
+}
