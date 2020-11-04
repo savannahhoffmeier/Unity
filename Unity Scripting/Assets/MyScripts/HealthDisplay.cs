100b678c9ee8a5668453920e376816d34bca2695
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-04 12:25:41 -0700

    health bar script

---
+++ Unity Scripting/Assets/MyScripts/HealthDisplay.cs
@@ -0,0 +1,19 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private int health = 5;
+    public Text healthText;
+    
+    void Update()
+    {
+        healthText.text = "HEALTH:" + health;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+        }
+    }
+}
