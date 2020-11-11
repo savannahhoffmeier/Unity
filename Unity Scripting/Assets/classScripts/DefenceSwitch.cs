809aeee773662dab1af4a11e4ccd5f092d1b6411
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-11 11:09:14 -0700

    class 11/10

---
+++ Unity Scripting/Assets/classScripts/DefenceSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class DefenceSwitch : MonoBehaviour
+{
+ public enum Defences
+ {
+  Sheild,
+  Potions,
+  ExtraHealth,
+  Buffs,
+  Supershield
+ }
+
+ public Defences currentDefences;
+
+ public void RunCurrentDefence()
+ {
+  switch (currentDefences)
+  {
+   case Defences.Sheild:
+    break;
+   case Defences.Potions:
+    break;
+   case Defences.ExtraHealth:
+    break;
+   case Defences.Buffs:
+    break;
+   case Defences.Supershield:
+    break;
+   default:
+    throw new ArgumentOutOfRangeException();
+  }
+ }
+}
---
+++ Unity Scripting/Assets/classScripts/GameSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[CreateAssetMenu]
+public class GameSwitch : ScriptableObject
+{
+    public enum GameStates
+    {
+        Starting,
+        Playing,
+        Ending,
+        InStore,
+        Pausing
+    }
+
+    public GameStates currentGameStates = GameStates.Starting;
+
+    public void ChangeToPlaying()
+    {
+        currentGameStates = GameStates.Playing;
+    }
+
+    public void RunCurrentState()
+    {
+        switch (currentGameStates)
+        {
+            case GameStates.Starting:
+                Debug.Log("Starting");
+                break;
+            case GameStates.Playing:
+                Debug.Log("Playing");
+                break;
+            case GameStates.Ending:
+                Debug.Log("Playing");
+                break;
+            case GameStates.InStore:
+                Debug.Log("InStore");
+                break;
+            case GameStates.Pausing:
+                Debug.Log("Pausing");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
