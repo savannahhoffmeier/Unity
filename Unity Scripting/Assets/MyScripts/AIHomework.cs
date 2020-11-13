432f6d3d3e52572f563b38d217da9ba80c96ddf0
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-13 14:00:45 -0700

    AiHomework&CharacterSwitch

---
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIHomework : MonoBehaviour
+{
+    private NavMeshAgent Spy;
+    public Transform Target;
+
+    private void Start()
+    {
+        Spy = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Spy.destination = Target.position;
+    }
+}
---
+++ Unity Scripting/Assets/MyScripts/CharacterSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitch : MonoBehaviour
+{
+    public enum Character
+    {
+        Girl,
+        Boy,
+        Nonbinary,
+        dog,
+        cat
+    }
+
+    public Character currentSwitch;
+
+    public void runCurrentSwitch()
+    {
+        switch (currentSwitch)
+        {
+            case Character.Girl:
+                break;
+            case Character.Boy:
+                break;
+            case Character.Nonbinary:
+                break;
+            case Character.dog:
+                break;
+            case Character.cat:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
