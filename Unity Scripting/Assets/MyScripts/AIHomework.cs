5d20d693914c5f9973128643628334d94cf982a7
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-18 11:39:07 -0700

    AI patrolling

--- Unity Scripting/Assets/MyScripts/AIHomework.cs
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -1,19 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIHomework : MonoBehaviour
 {
     private NavMeshAgent Spy;
-    public Transform Target;
+    private Transform destination;
+    private bool patrol;
+    private int i = 0;
+
+    public List<Transform> patrolPoints;
 
     private void Start()
     {
         Spy = GetComponent<NavMeshAgent>();
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        patrol = false;
+        destination = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        patrol = true;
+    }
+    
+
     private void Update()
     {
-        Spy.destination = Target.position;
+        Spy.destination = destination.position;
+        if (!patrol) return;
+        {
+            destination = patrolPoints[i];
+        }
     }
 }
