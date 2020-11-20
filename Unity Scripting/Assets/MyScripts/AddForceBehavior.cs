0ddd6cfbcea7641d33c72d7429575e499064a652
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-20 13:09:05 -0700

    Delay

---
+++ Unity Scripting/Assets/MyScripts/AddForceBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class AddForceBehavior : MonoBehaviour
+{
+ private Rigidbody rigid;
+ public Vector3 forces;
+ public float holdTime=3f;
+
+ private IEnumerator Start()
+ {
+  rigid = GetComponent<Rigidbody>();
+  yield return new WaitForSeconds(holdTime);
+  rigid.AddForce(forces);
+  rigid = GetComponent<Rigidbody>();
+  yield return new WaitForSeconds(holdTime);
+  rigid.AddForce(forces);
+  rigid = GetComponent<Rigidbody>();
+  yield return new WaitForSeconds(holdTime);
+  rigid.AddForce(forces);
+  rigid = GetComponent<Rigidbody>();
+  yield return new WaitForSeconds(holdTime);
+  rigid.AddForce(forces);
+ }
+}
