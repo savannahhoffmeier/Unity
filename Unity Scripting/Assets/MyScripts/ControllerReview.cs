794046a6c291e0b6227489337be8d6dc8f507cac
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-25 11:41:05 -0700

    CharacterControllerReview

---
+++ Unity Scripting/Assets/MyScripts/ControllerReview.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class ControllerReview : MonoBehaviour
+{
+    public float speed = 14f;
+    private Rigidbody rig;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection= Vector3.zero;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float haxis = Input.GetAxis("Horizontal");
+        float vaxis = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(haxis, 0, vaxis) * speed * Time.deltaTime;
+        rig.MovePosition(transform.position+movement);
+
+        if (controller.isGrounded && Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
