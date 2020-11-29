5157a3a193557187694a1d11574a35c6e51a5f58
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-28 22:16:16 -0700

    ColorChangingGradual

---
+++ Unity Scripting/Assets/MyScripts/ScriptTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTest : MonoBehaviour
+{
+public Color colorIni = Color.white;
+     public Color colorFin = Color.red;
+     public float duration = 3f;
+     Color lerpedColor = Color.white;
+ 
+     private float t=0;
+     private bool flag; 
+ 
+     Renderer _renderer;
+     
+     void Start () {
+         _renderer=GetComponent<Renderer>(); 
+     }
+     
+     void Update() {
+         lerpedColor = Color.Lerp(colorIni, colorFin,  t);
+         _renderer.material.color = lerpedColor;
+ 
+         if (flag == true) {
+             t -= Time.deltaTime/duration;
+             if (t < 0.01f)
+                 flag = false;
+         } else {
+             t += Time.deltaTime/duration;
+             if (t > 0.99f)
+                 flag = true;
+         }
+     }
+}
