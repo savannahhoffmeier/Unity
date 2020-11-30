099cdf57e6e49aec6c743f5663eefae463d18041
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-30 11:35:31 -0700

    Review Prev Class

--- Unity Scripting/Assets/MyScripts/Review.cs
+++ Unity Scripting/Assets/MyScripts/Review.cs
@@ -1,38 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class Review : MonoBehaviour
 {
- public Color skinColor = Color.blue;
- private NavMeshAgent Baby;
- private Transform destination;
- private bool patrol;
- private int i = 0;
- public List<Transform> patrolPoints;
- private void Start()
- {
-  Baby = GetComponent<NavMeshAgent>();
- }
- private void OnTriggerEnter(Collider other)
- {
-  patrol = false;
-  destination = other.transform;
- }
- private void OnTriggerExit(Collider other)
- {
-  patrol = true;
- }
- private void Update()
- {
-  Baby.destination = destination.position;
-  if (!patrol) return;
-  {
-   destination = patrolPoints[i];
-  }
- }
-
+    private Rigidbody _rigidbody;
+    public Vector3 forces, forces2;
+    public float holdTime = 3f;
+    public int counter = 10;
+    public bool canRun;
+    private IEnumerator Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        while (counter>0)
+        {
+            yield return new WaitForSeconds(holdTime);
+            _rigidbody.AddForce(forces);
+            counter--;
+        }
+        while (canRun)
+        {
+            yield return new WaitForSeconds(holdTime);
+            _rigidbody.AddForce(forces2);
+            counter--;
+        }
+    }
 }
