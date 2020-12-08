bdd2da3ac25485cc0215d07bb38997d283e76cde
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-12-08 15:46:54 -0700

    class Q review

--- Unity Scripting/Assets/classScripts/MonoEvents.cs
+++ Unity Scripting/Assets/classScripts/MonoEvents.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.WSA.Input;
 
 public class MonoEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public float holdTime = 3f;
 }
---
+++ Unity Scripting/Assets/classScripts/ReviewCharacterMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class ReviewCharacterMover : MonoBehaviour
+{
+ 
+    private CharacterController player;
+    public float moveSpeed = 5f, gravity = -9.81f, jumpForce = 10f;
+    private Vector3 moveDirection;
+    private float yDirection;
+    private void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        moveDirection.Set(moveSpeedInput,yDirection,0);
+        yDirection += gravity * Time.deltaTime;
+        if(player.isGrounded&& moveDirection.y<0)
+        {
+            yDirection = -1f;
+        }
+        var moveSpeedInput2 = moveSpeed * Input.GetAxis("Vertical");
+        moveDirection.Set(moveSpeedInput2,yDirection,0);
+        yDirection += gravity * Time.deltaTime;
+        if(player.isGrounded&& moveDirection.y<0)
+        {
+            yDirection = -1f;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection = jumpForce;
+        }
+
+        var movement = moveDirection * Time.deltaTime;
+            player.Move(movement);
+    }
+}
