e0b6e8b8f905735d3b39ce3d203a1300cc766f6c
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-12-10 15:35:28 -0700

    Class Review

---
+++ Unity Scripting/Assets/classScripts/FunctionOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionOptions : MonoBehaviour
+{
+    public int number;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = MathExample(7);
+    }
+
+    public int MathExample(int value)
+    {
+        return value + 3;
+    }
+ 
+}
---
+++ Unity Scripting/Assets/classScripts/ScriptTesting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ScriptTesting : MonoBehaviour
+{
+    private CharacterController player;
+    public float Movement = 6f, gravity = 30f, jumpForce = 30;
+    private Vector3 moving;
+    private float yDirect;
+
+    private void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var moveSpeedInput = Movement * Input.GetAxis("Horizontal");
+        moving.Set(moveSpeedInput, yDirect, 0);
+        yDirect += gravity * Time.deltaTime;
+        if (player.isGrounded && moving.y < 0)
+        {
+            yDirect = -1f;
+        }
+
+        var moveSpeed = Movement * Input.GetAxis("Vertical");
+        moving.Set(moveSpeedInput, yDirect, 3);
+        yDirect += gravity * Time.deltaTime;
+        if (player.isGrounded && moving.y < 0)
+        {
+            yDirect = -1f;
+        }
+    }
+}
