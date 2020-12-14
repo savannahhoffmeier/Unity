90bd484d6e4fe3845699fdb3dd77ed626fdf1c58
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-12-14 12:16:17 -0700

    FIxing List Problems

---
+++ Unity Scripting/Assets/classScripts/Collectibles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    public List<Transform> collectedTransforms;
+    public Sprite art;
+    public Color artColor;
+
+    public void Collect(Transform obj)
+    {
+        collectedTransforms.Add(obj);
+    }
+
+    public void PrintCollection()
+    {
+        foreach (var obj in collectedTransforms)
+        {
+            Debug.Log(obj);   
+        }
+    }
+}
---
+++ Unity Scripting/Assets/classScripts/Collections.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections : MonoBehaviour
+{
+    public List<Collectibles> collectibleList;
+
+    private void Start()
+    {
+        for (var i = 0; i < collectibleList.Count; i++)
+        {
+            var position= new Vector3(i*2,0,0);
+            var item = collectibleList[i];
+            var newItem = new GameObject(item.name);
+            newItem.transform.position = position;
+            var sprite = newItem.AddComponent<SpriteRenderer>();
+            sprite.sprite = item.art;
+            sprite.color = item.artColor;
+        }
+    }
+
+    public void RemoveRedItemsFromCollection()
+    {
+        for (int i = 0; i < collectibleList.Count; i++)
+        {
+            if (collectibleList[i].artColor==Color.red)
+            {
+                collectibleList.RemoveAt(i);
+            }
+        }
+    }
+}
