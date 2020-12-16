55bc52360a2b983df6e71d9714c6317b77c0cc57
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-12-15 17:00:07 -0700

    Last Class Review

---
+++ Unity Scripting/Assets/classScripts/BulletBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class BulletBehaviour : MonoBehaviour
+{
+  private Rigidbody rBody;
+  public float bulletForce= 500f;
+  public float lifeTime=4f;
+
+  private IEnumerator Start()
+  {
+    rBody = GetComponent<Rigidbody>();
+    rBody.AddForce(transform.forward*bulletForce);
+    
+    yield return new WaitForSeconds(lifeTime);
+    Destroy(gameObject);
+  }
+}
---
+++ Unity Scripting/Assets/classScripts/WitchShooting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class WitchShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform target;
+    private float fireRate;
+    private float nextFire;
+
+    private void Start()
+    {
+        fireRate = 1f;
+        nextFire = Time.time;
+    }
+
+    private void Update()
+    {
+        LookAt();
+        if (Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            nextFire = Time.time + fireRate;
+        }
+        
+    }
+
+    private void LookAt()
+    {
+        transform.LookAt(target);
+    }
+}
