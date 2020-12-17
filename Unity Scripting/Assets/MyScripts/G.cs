7757eab6ee2e662126a0197cfb85ac8c83596a8a
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-12-16 23:05:17 -0700

    I got it, oh and scripted a game Switch

---
+++ Unity Scripting/Assets/MyScripts/G.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G : MonoBehaviour
+{
+    private GameObject player;
+    public enum Gamepoints
+    {
+        Start,
+        Play,
+        GameOver,
+        Store,
+        Pause
+    }
+
+    public Gamepoints CurrentGamepoints = Gamepoints.Start;
+
+    public void Playing()
+    {
+        CurrentGamepoints = Gamepoints.Play;
+    }
+
+    public void RunCurrentState()
+    {
+        switch (CurrentGamepoints)
+        {
+            case Gamepoints.Start:
+                Debug.Log("Game Start");
+                break;
+            case Gamepoints.Play:
+                Debug.Log("Play");
+                break;
+            case Gamepoints.GameOver:
+                Debug.Log("Game Over");
+                Destroy(player);
+                break;
+            case Gamepoints.Store:
+                Debug.Log("Storage");
+                break;
+            case Gamepoints.Pause:
+                Debug.Log("Game Paused");
+                break;
+        }
+    }
+}
