53152046448c6c5c39a7da58d77796ae6f058552
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-12-17 15:39:56 -0700

    Destroy Coding

---
+++ Unity Scripting/Assets/MyScripts/UnityTutorial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityTutorial : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(gameObject, 3f);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Destroy(gameObject);
+            rb.AddForce(Vector3.up*500);
+        }
+        rb.velocity=Vector3.forward*20f;
+    }
+
+    private void OnMouseDown()
+    {
+        //Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
