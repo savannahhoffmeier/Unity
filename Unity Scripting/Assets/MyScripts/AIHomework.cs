5c31045dc006204d3b73ec1b57531933fdaa65de
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-05 11:06:22 -0700

    Setting up AI Script

--- Unity Scripting/Assets/MyScripts/AIHomework.cs
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -2,40 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIHomework : MonoBehaviour
 {
-    private NavMeshAgent Spy;
-    private Transform destination;
-    private bool patrol;
-    private int i = 0;
+  public NavMeshAgent agent;
+  public Transform player;
+  public LayerMask whatIsGround, whatIsPlayer;
+  
+  public Vector3 walkPoint;
+  private bool walkPointSet;
+  public float walkPointRange;
+  public float timeBetweenAttacks;
+  private bool alreadyAttacked;
 
-    public List<Transform> patrolPoints;
+  public float sightRange, attackRange;
+  public bool playerInSightRange, playInAttackRange;
 
-    private void Start()
-    {
-        Spy = GetComponent<NavMeshAgent>();
-    }
+  private void Awake()
+  {
+    player = GameObject.Find("PlayerObj").transform;
+    agent = GetComponent<NavMeshAgent>();
+  }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        patrol = false;
-        destination = other.transform;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        patrol = true;
-    }
+  private void Patroling()
+  {
     
+  }
 
-    private void Update()
-    {
-        Spy.destination = destination.position;
-        if (!patrol) return;
-        {
-            destination = patrolPoints[i];
-        }
-    }
+  private void ChasePlayer()
+  {
+    
+  }
+
+  private void AttackPlayer()
+  {
+    
+  }
 }
