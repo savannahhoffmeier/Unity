17ebe2e94bbcedc6370c4be8753cda3e386b016e
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-06 11:05:52 -0700

    Preparing Ai Script

--- Unity Scripting/Assets/MyScripts/AIHomework.cs
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIHomework : MonoBehaviour
 {
@@ -16,7 +18,7 @@ public class AIHomework : MonoBehaviour
   private bool alreadyAttacked;
 
   public float sightRange, attackRange;
-  public bool playerInSightRange, playInAttackRange;
+  public bool playerInSightRange, playerInAttackRange;
 
   private void Awake()
   {
@@ -24,11 +26,37 @@ public class AIHomework : MonoBehaviour
     agent = GetComponent<NavMeshAgent>();
   }
 
-  private void Patroling()
+  private void Update()
   {
-    
+    playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+    playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+    if (!playerInSightRange && !playerInAttackRange) Patroling();
+    if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+    if (playerInAttackRange && playerInSightRange) AttackPlayer();
   }
 
+  private void Patroling()
+  {
+    if (!walkPointSet) SearchWalkPoint();
+    if (walkPointSet)
+    {
+      agent.SetDestination(walkPoint);
+    }
+
+    Vector3 distanceToWalkPoint = transform.position - walkPoint;
+  }
+
+  private void SearchWalkPoint()
+  {
+    float randomZ = Random.Range(-walkPointRange, walkPointRange);
+    float randomx = Random.Range(-walkPointRange, walkPointRange);
+    
+    walkPoint= new Vector3(transform.position.x+randomx, transform.position.y, transform.position.z + randomZ);
+    if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+    {
+      walkPointSet = true;
+    }
+  }
   private void ChasePlayer()
   {
     
