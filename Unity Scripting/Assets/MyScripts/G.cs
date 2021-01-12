35e1d0431737f389aea43e96bf5a13b0caab5fa7
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-12 14:49:17 -0700

    Writing Shooting Script

--- Unity Scripting/Assets/MyScripts/G.cs
+++ Unity Scripting/Assets/MyScripts/G.cs
@@ -26,6 +26,11 @@ public class G : MonoBehaviour
         if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range))
         {
             Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target !=null)
+            {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
