75a748cf9f78d856bf628625dd1d224a0598e53d
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-13 18:28:44 -0700

    FInish Fire Script

--- Unity Scripting/Assets/MyScripts/G.cs
+++ Unity Scripting/Assets/MyScripts/G.cs
@@ -8,20 +8,26 @@ using UnityEngine.Events;
 
 public class G : MonoBehaviour
 {
+    public ParticleSystem muzzleFlash;
     public float damage = 10f;
     public float range = 100f;
     public Camera fpsCam;
-
+    public GameObject impacteffect;
+    public float impactforce = 30f;
+    public float fireRate = 15f;
+    private float nextTimeFire = 0f;
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1")&& Time.time>=nextTimeFire)
         {
+            nextTimeFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
     void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range))
         {
@@ -31,6 +37,13 @@ public class G : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal*impactforce);
+            }
+            GameObject impactGo =Instantiate(impacteffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo);
         }
     }
 }
