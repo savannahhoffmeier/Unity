461b7cdcc2eb4888554a5cf8402f389ba1804235
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-17 12:09:36 -0700

    First Person Movement Script

--- Unity Scripting/Assets/MyScripts/Target.cs
+++ Unity Scripting/Assets/MyScripts/Target.cs
@@ -1,22 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Target : MonoBehaviour
 {
-   public float health = 50f;
+    public CharacterController controller;
 
-   public void TakeDamage(float amount)
-   {
-      health -= amount;
-      if (health<=0f)
-      {
-         Die();
-      }
-   }
+    public float speed = 12f;
+    public float gravity = -9.81f;
 
-   void Die()
-   {
-      Destroy(gameObject);
-   }
+    private Vector3 velocity;
+    
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
