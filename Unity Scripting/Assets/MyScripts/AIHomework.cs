1914aeae91b45ab3660ab350547c73b60923eee0
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-19 14:42:46 -0700

    Jumping Script

--- Unity Scripting/Assets/MyScripts/AIHomework.cs
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -5,5 +5,40 @@ using UnityEngine.AI;
 using Random = UnityEngine.Random;
 public class AIHomework : MonoBehaviour
 {
-  
+   public float jumpingHeight = 7f;
+   public bool Grounded;
+   
+   private Rigidbody rb;
+   
+   void Start()
+   {
+    rb = GetComponent<Rigidbody>();
+   }
+   
+   void Update()
+   {
+      if (Grounded)
+      {
+         if (Input.GetButtonDown("Jump"))
+         {
+             rb.AddForce(Vector3.up * jumpingHeight);
+         }
+      }
+   }
+   
+   void OnCollisionEnter(Collision other)
+   {
+       if (other.gameObject.tag == "Ground")
+       {
+           Grounded = true;
+       }
+   }
+   
+   void OnCollisionExit(Collision other)
+   {
+       if (other.gameObject.tag == "Ground")
+       {
+           Grounded = false;
+       }
+   }
 }
