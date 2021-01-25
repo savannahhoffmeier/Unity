ac7001c51e00b4f2216b02d2c03a66dd6cb1ec51
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-25 16:36:41 -0700

    CHaracter controller Review

--- Unity Scripting/Assets/MyScripts/ControllerReview.cs
+++ Unity Scripting/Assets/MyScripts/ControllerReview.cs
@@ -7,27 +7,41 @@ using UnityEngine;
 
 public class ControllerReview : MonoBehaviour
 {
-  public int MaxHealth = 100;
-  public int CurrentHealth;
-  public Health healthBar;
-
-  private void Start()
-  {
-    CurrentHealth = MaxHealth;
-    healthBar.SetMaxHealth(MaxHealth);
-  }
-
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      TakeDamage(20);
-    }
-
-    void TakeDamage(int damage)
-    {
-      CurrentHealth -= damage;
-      healthBar.SetHealth(CurrentHealth);
-    }
-  }
+  private CharacterController controller;
+      private Vector3 Velocity;
+      private bool Player;
+      private float Speed = 2.0f;
+      private float jumpHeight = 1.0f;
+      private float gravityValue = -9.81f;
+  
+      private void Start()
+      {
+          controller = gameObject.AddComponent<CharacterController>();
+      }
+  
+      void Update()
+      {
+          Player = controller.isGrounded;
+          if (Player && Velocity.y < 0)
+          {
+              Velocity.y = 0f;
+          }
+  
+          Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+          controller.Move(move * Time.deltaTime * Speed);
+  
+          if (move != Vector3.zero)
+          {
+              gameObject.transform.forward = move;
+          }
+  
+          // Changes the height position of the player..
+          if (Input.GetButtonDown("Jump") && Player)
+          {
+              Velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+          }
+  
+          Velocity.y += gravityValue * Time.deltaTime;
+          controller.Move(Velocity * Time.deltaTime);
+      }
 }
