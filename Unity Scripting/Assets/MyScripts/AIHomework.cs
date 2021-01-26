23f4a0906151a34e7dec3cd3463533c7a887e48a
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-26 14:28:23 -0700

    Testing different AI scrpts

--- Unity Scripting/Assets/MyScripts/AIHomework.cs
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
+
 [RequireComponent(typeof(Rigidbody))]
 public class AIHomework : MonoBehaviour
 {
@@ -18,12 +19,35 @@ public class AIHomework : MonoBehaviour
     private void Update()
     {
         Vector3 direction = player.position - this.transform.position;
-        float angle = Vector3.Angle(direction,this.transform.forward);
-        if (Vector3.Distance(player.position,this.transform.position)<10&&angle<30)
+        float angle = Vector3.Angle(direction, this.transform.forward);
+        if (Vector3.Distance(player.position, this.transform.position) < 10 && angle < 30)
         {
             direction.y = 0;
-            
-            this.transform.rotation=Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction), 0.1f);
+
+            this.transform.rotation =
+                Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            anime.SetBool("isIdle", false);
+            if ((direction.magnitude > 5))
+            {
+                this.transform.Translate(0, 0, 0.10f);
+                anime.SetBool("isRunning", true);
+                anime.SetBool("isAttacking", false);
+                anime.SetBool("isTalking", false);
+                anime.SetBool("isIdle2", false);
+
+            }
+            else
+            {
+                anime.SetBool("isRunning", false);
+                anime.SetBool("isAttacking", true);
+                anime.SetBool("isTalking", false);
+                anime.SetBool("isIdle2", false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anime.SetBool("isTalking", true);
+            }
         }
     }
 }
