750f4d43a32055186c5c16bd8d56032fbd7db208
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-01-31 13:05:53 -0700

    Created a AddForce

--- Unity Scripting/Assets/MyScripts/ColorChange.cs
+++ Unity Scripting/Assets/MyScripts/ColorChange.cs
@@ -1,8 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class ColorChange : MonoBehaviour
 {
-  
+    private Rigidbody rigid;
+    public Vector3 forces;
+    public float holdTime=3f;
+
+    private IEnumerator Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(holdTime);
+        rigid.AddForce(forces);
+        rigid = GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(holdTime);
+        rigid.AddForce(forces);
+        rigid = GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(holdTime);
+        rigid.AddForce(forces);
+        rigid = GetComponent<Rigidbody>();
+        yield return new WaitForSeconds(holdTime);
+        rigid.AddForce(forces);
+    }
 }
--- Unity Scripting/Assets/MyScripts/Target.cs
+++ Unity Scripting/Assets/MyScripts/Target.cs
@@ -6,23 +6,4 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Target : MonoBehaviour
 {
-    public CharacterController controller;
-
-    public float speed = 12f;
-    public float gravity = -9.81f;
-
-    private Vector3 velocity;
-    
-    private void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-    }
-}
+    p }
