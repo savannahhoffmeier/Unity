c8ef84c664715cf54f460c808ecfc0cfd8f809e3
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-02-07 10:57:04 -0700

    random color changing script

--- Unity Scripting/Assets/MyScripts/CharacterSwitch.cs
+++ Unity Scripting/Assets/MyScripts/CharacterSwitch.cs
@@ -2,36 +2,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Random))]
 
 public class CharacterSwitch : MonoBehaviour
 {
-    public enum Character
+    private int rNumber;
+    public new Light light;
+    public float redo = 2f;
+    private float time = 0;
+ 
+    private void Start()
     {
-        Girl,
-        Boy,
-        Nonbinary,
-        dog,
-        cat
+        light = gameObject.GetComponent<Light>();
+        rNumber = Random.Range(1, 4);
     }
-
-    public Character currentSwitch;
-
-    public void runCurrentSwitch()
+    private void Update()
     {
-        switch (currentSwitch)
+        time += Time.deltaTime ;
+        if (time >= redo)
         {
-            case Character.Girl:
-                break;
-            case Character.Boy:
-                break;
-            case Character.Nonbinary:
-                break;
-            case Character.dog:
-                break;
-            case Character.cat:
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
+            switch (rNumber)
+            {
+                case 0:
+                    break;
+                case 1:
+                    light.color = Color.blue;
+                    Debug.Log("Color switched to blue!");
+                    break;
+                case 2:
+                    light.color = Color.red;
+                    Debug.Log("Color switched to red!");
+                    break;
+                case 3:
+                    light.color = Color.green;
+                    Debug.Log("Color switched to green!");
+                    break;
+            }
+            time = 0f;
+            rNumber = Random.Range(1, 4);
         }
     }
 }
