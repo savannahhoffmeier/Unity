5715d00032be49100b95ac7c6495d7b15ea7b784
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-02-10 15:19:26 -0700

    character controller online script test

--- Unity Scripting/Assets/classScripts/CharacterMover_1.cs
+++ Unity Scripting/Assets/classScripts/CharacterMover_1.cs
@@ -4,41 +4,25 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMover_1 : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 Velocity;
-    private bool Player;
-    private float Speed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
-
-    private void Start()
+    bool canPlayerMove = true;
+  
+    public float speed = 0.0f;
+  
+    void FixedUpdate()
     {
-        controller = gameObject.AddComponent<CharacterController>();
-    }
-
-    void Update()
-    {
-        Player = controller.isGrounded;
-        if (Player && Velocity.y < 0)
+        float moveHorizontal = -Input.GetAxisRaw("LeftJoyStickHorizontal");
+        float moveVertical = Input.GetAxisRaw("LeftJoyStickVertical");
+  
+        if (canPlayerMove)
         {
-            Velocity.y = 0f;
+            Vector3 movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
+  
+            if (movement != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+            }
+  
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
         }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * Speed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && Player)
-        {
-            Velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        Velocity.y += gravityValue * Time.deltaTime;
-        controller.Move(Velocity * Time.deltaTime);
     }
 }
