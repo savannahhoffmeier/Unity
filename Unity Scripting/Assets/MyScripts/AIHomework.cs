944be3ae92526248d00708f14243ff6b19099fee
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-02-17 16:12:31 -0700

    Created a string list data

--- Unity Scripting/Assets/MyScripts/AIHomework.cs
+++ Unity Scripting/Assets/MyScripts/AIHomework.cs
@@ -3,66 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
+[CreateAssetMenu]
 
 [RequireComponent(typeof(Rigidbody))]
 public class AIHomework : MonoBehaviour
 {
-    private Rigidbody rigid;
-    public Vector3 forces;
-    public float holdTime=3f;
-    public enum GameStates
+    public List<string> value;
+    private int indexer = 0;
+
+    private void OnEnable()
     {
-        Starting,
-        Playing,
-        Ending,
-        InStore,
-        Pausing
+        indexer = 0;
     }
 
-    public GameStates currentGameStates = GameStates.Starting;
-
-    public void ChangeToPlaying()
+    public void UpdateText(Text txt)
     {
-        currentGameStates = GameStates.Playing;
-    }
-
-    public void RunCurrentState()
-    {
-        switch (currentGameStates)
-        {
-            case GameStates.Starting:
-                Debug.Log("Starting");
-                break;
-            case GameStates.Playing:
-                Debug.Log("Playing");
-                break;
-            case GameStates.Ending:
-                Debug.Log("Playing");
-                break;
-            case GameStates.InStore:
-                Debug.Log("InStore");
-                break;
-            case GameStates.Pausing:
-                Debug.Log("Pausing");
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    } 
-    private IEnumerator Start()
-    {
-        rigid = GetComponent<Rigidbody>();
-        yield return new WaitForSeconds(holdTime);
-        rigid.AddForce(forces);
-        rigid = GetComponent<Rigidbody>();
-        yield return new WaitForSeconds(holdTime);
-        rigid.AddForce(forces);
-        rigid = GetComponent<Rigidbody>();
-        yield return new WaitForSeconds(holdTime);
-        rigid.AddForce(forces);
-        rigid = GetComponent<Rigidbody>();
-        yield return new WaitForSeconds(holdTime);
-        rigid.AddForce(forces);
+        txt.text = value[indexer];
+        indexer = (indexer + 1) % value.Count;
     }
 }
