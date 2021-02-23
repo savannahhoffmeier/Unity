ba8b8ad4051a0efc16156b4d3e1d31562d351214
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-02-23 11:27:20 -0700

    Small Review

--- Unity Scripting/Assets/MyScripts/CharacterSwitch.cs
+++ Unity Scripting/Assets/MyScripts/CharacterSwitch.cs
@@ -10,25 +10,25 @@ using Random = UnityEngine.Random;
 
 public class CharacterSwitch : MonoBehaviour
 {
-    bool canPlayerMove = true;
-  
-    public float speed = 0.0f;
-  
-    void FixedUpdate()
+    public int score;
+    public float health;
+    public string playerName;
+    public bool canPlay;
+    public GameObject playerArt;
+    public Transform playerTransform;
+
+    private CharacterController _controller;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
     {
-        float moveHorizontal = -Input.GetAxisRaw("LeftJoyStickHorizontal");
-        float moveVertical = Input.GetAxisRaw("LeftJoyStickVertical");
-  
-        if (canPlayerMove)
-        {
-            Vector3 movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
-  
-            if (movement != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
-            }
-  
-            transform.Translate(movement * speed * Time.deltaTime, Space.World);
-        }
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = 3f;
+        _controller.Move(movement);
     }
 }
