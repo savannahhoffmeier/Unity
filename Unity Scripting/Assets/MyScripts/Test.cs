fad72551e777f85bc0e2bba6c58c5ebd3e4a17e6
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-02-24 16:38:57 -0700

    Following Youtube Review

---
+++ Unity Scripting/Assets/MyScripts/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Test : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("Space"))
+        {
+            rb.AddForce(Vector3.up);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
