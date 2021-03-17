371d2374a8540ed51fcaaea54760853c68a80219
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-03-17 15:52:31 -0600

    Collections Script

--- Unity Scripting/Assets/MyScripts/Test.cs
+++ Unity Scripting/Assets/MyScripts/Test.cs
@@ -9,32 +9,19 @@ using Random = UnityEngine.Random;
 
 public class Test : MonoBehaviour
 {
-    public GameObject Arrow;
-    public GameObject Bow;
-    public static int movespeed = 3;
-    public Vector3 userDirection = Vector3.down;
-    
-    void Start()
-    {
-     (Arrow) = GameObject.Find("Arrow"); 
-     (Bow) = GameObject.Find("Bow"); 
-     Arrow.transform.position = Bow.transform.position; 
-    }
-    void Update()
-    {
-        transform.Translate(userDirection * movespeed * Time.deltaTime);
-        
-        if (Input.GetMouseButtonDown(0)) {
-            gameObject.active = true;
-        }
-        if (Input.GetMouseButtonDown(1)) {
-            gameObject.active = false;
-        }
+    public List<Transform> collectedTransforms;
+    public Sprite art;
+    public Color artColor;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            Destroy(gameObject);
-        }
+    public void Collect(Transform obj)
+    {
+        collectedTransforms.Add(obj);
     }
-    
-}
+
+    public void PrintCollection()
+    {
+        foreach (var obj in collectedTransforms)
+        {
+            Debug.Log(obj);   
+        }
+    } }
