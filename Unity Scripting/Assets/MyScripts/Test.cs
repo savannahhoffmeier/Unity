5ffaff8d82405293937474cbd26eb0a6ac3f4b4a
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-03-18 15:57:02 -0600

    collectibles script

--- Unity Scripting/Assets/MyScripts/Test.cs
+++ Unity Scripting/Assets/MyScripts/Test.cs
@@ -9,19 +9,21 @@ using Random = UnityEngine.Random;
 
 public class Test : MonoBehaviour
 {
-    public List<Transform> collectedTransforms;
-    public Sprite art;
-    public Color artColor;
-
-    public void Collect(Transform obj)
-    {
-        collectedTransforms.Add(obj);
-    }
-
-    public void PrintCollection()
-    {
-        foreach (var obj in collectedTransforms)
-        {
-            Debug.Log(obj);   
+    
+   public List<Transform> collectedTransforms;
+       public Sprite art;
+       public Color artColor;
+   
+       public void Collect(Transform obj)
+       {
+           collectedTransforms.Add(obj);
+       }
+   
+       public void PrintCollection()
+       {
+           foreach (var obj in collectedTransforms)
+           {
+               Debug.Log(obj);   
+           }
+       }
         }
-    } }
