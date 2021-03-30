aa794b26cbeea9a1d09b601276667c67b0937b7f
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-03-29 19:04:05 -0600

    became really sick did simple character controller

--- Unity Scripting/Assets/MyScripts/Test.cs
+++ Unity Scripting/Assets/MyScripts/Test.cs
@@ -9,86 +9,41 @@ using Random = UnityEngine.Random;
 
 public class Test : MonoBehaviour
 {
-    public enum ShootState {
-        Ready,
-        Shoot,
-        Reload
-    }
-    
-    private float muzzleOffset;
-    [Header("Magazine")]
-    public GameObject round;
-    public int ammo;
-    [Range(0.5f, 10)] public float reloadTime;
-    private int remainingAmmo;
-    [Header("Shooting")]
-    [Range(0.25f, 25)] public float fireRate;
-    public int roundsPerShot;
-    [Range(0.5f, 100)] public float roundSpeed;
-    [Range(0, 45)] public float maxRoundVariation;
-    private ShootState shootState = ShootState.Ready;
-    private float nextShootTime = 0;
+    private CharacterController controller;
+    private Vector3 playerV;
+    private bool gPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
-    void Start() {
-        muzzleOffset = GetComponent<Renderer>().bounds.extents.z;
-        remainingAmmo = ammo;
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
     }
-    void Update() {
-        switch(shootState) {
-            case ShootState.Shoot:
-                
-                if(Time.time > nextShootTime) {
-                    shootState = ShootState.Ready;
-                }
-                break;
-            case ShootState.Reload:
-               
-                if(Time.time > nextShootTime) {
-                    remainingAmmo = ammo;
-                    shootState = ShootState.Ready;
-                }
-                break;
+
+    void Update()
+    {
+        gPlayer = controller.isGrounded;
+        if (gPlayer && playerV.y < 0)
+        {
+            playerV.y = 0f;
         }
-    }
-    public void Shoot() {
-        // Checks that the gun is ready to shoot
-        if(shootState == ShootState.Ready) {
-            for(int i = 0; i < roundsPerShot; i++) {
-                // Instantiates the round at the muzzle position
-                GameObject spawnedRound = Instantiate(
-                    round,
-                    transform.position + transform.forward * muzzleOffset,
-                    transform.rotation
-                );
 
-                // Add a random variation to the round's direction
-                spawnedRound.transform.Rotate(new Vector3(
-                    Random.Range(-1f, 1f) * maxRoundVariation,
-                    Random.Range(-1f, 1f) * maxRoundVariation,
-                    0
-                ));
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
 
-                Rigidbody rb = spawnedRound.GetComponent<Rigidbody>();
-                rb.velocity = spawnedRound.transform.forward * roundSpeed;
-            }
-
-            remainingAmmo--;
-            if(remainingAmmo > 0) {
-                nextShootTime = Time.time + (1 / fireRate);
-                shootState = ShootState.Shoot;
-            } else {
-                Reload();
-            }
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
         }
-    }
 
-    /// Attempts to reload the gun
-    public void Reload() {
-        // Checks that the gun is ready to be reloaded
-        if(shootState == ShootState.Ready) {
-            nextShootTime = Time.time + reloadTime;
-            shootState = ShootState.Reload;
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && gPlayer)
+        {
+            playerV.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
-    }
 
+        playerV.y += gravityValue * Time.deltaTime;
+        controller.Move(playerV * Time.deltaTime);
+    }
 }
