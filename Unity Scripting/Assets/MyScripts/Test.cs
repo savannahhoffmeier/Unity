50b9c27dcf10ad038684b5a02d6af3e263a51db1
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-04-05 17:40:52 -0600

    Follow player script

--- Unity Scripting/Assets/MyScripts/Test.cs
+++ Unity Scripting/Assets/MyScripts/Test.cs
@@ -1,52 +1,17 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.Mime;
-using System.Security.Cryptography;
-using UnityEngine;
-using UnityEngine.UI;
-using Random = UnityEngine.Random;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Test : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerV;
-    private bool gPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
-
-    private void Start()
+    float movementSpeed = 100f;
+    private Rigidbody rb;
+    private void FaceTarget(Vector3 destination)
     {
-        controller = gameObject.AddComponent<CharacterController>();
-    }
-
-    void Update()
-    {
-        gPlayer = controller.isGrounded;
-        if (gPlayer && playerV.y < 0)
-        {
-            playerV.y = 0f;
-        }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-    
-      {
-              float amountToMove = movementSpeed * Time.deltaTime;
-              Vector3 movement = (Input.GetAxis("Horizontal") * -Vector3.left * amountToMove) + (Input.GetAxis("Vertical") * Vector3.forward * amountToMove);
-              rigidbody.AddForce(movement, ForceMode.Force);
-       
-              if (Input.GetKeyDown("space"))
-              {
-                  rigidbody.AddForce(Vector3.up * jumpSpeed);
-              }
-
-        playerV.y += gravityValue * Time.deltaTime;
-        controller.Move(playerV * Time.deltaTime);
+        Vector3 lookPos = destination - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 15);  
+        rb.GetComponent<Rigidbody>().velocity = transform.forward * Time.deltaTime * movementSpeed;
+        
     }
 }
