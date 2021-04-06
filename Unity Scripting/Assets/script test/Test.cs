d5718e1670d9bd90ff2a397ff4e357a83223f6b4
Savannah Hoffmeier <10858457@my.uvu.edu>
2021-04-06 17:37:52 -0600

    Made a different follow player script that worked

---
+++ Unity Scripting/Assets/script test/Test.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Test : MonoBehaviour
+{
+ public float attackSpeed = 4;
+     public float attackDistance;
+     public float waitDistance;
+     public GameObject player;
+ 
+     Transform playerTransform;
+ 
+     void GetPlayerTransform()
+     {
+         if (player != null)
+         {
+             playerTransform = player.transform;
+         }
+         else
+         {
+             Debug.Log("Player is seen");
+         }
+     }
+ 
+     // Start is called before the first frame update
+     void Start()
+     {
+         GetPlayerTransform();
+     }
+ 
+     // Update is called once per frame
+     void Update()
+     {
+         var distance = Vector3.Distance(playerTransform.position, transform.position);
+         
+         if (distance <= attackDistance)
+         {
+             if (distance >= waitDistance)
+             {
+                 transform.position += transform.forward * attackSpeed * Time.deltaTime;
+             }
+         }
+     }
+}
